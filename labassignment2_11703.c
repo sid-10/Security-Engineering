8a2d39e970074140cf16b77ce6780d878a307a78
sid-10 <siddharth14104@iiitd.ac.in>
2017-07-19 14:38:00 +0530

    Final Projects

---
+++ labassignment2_11703.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+        char name[64];
+        
+        memset(name,0,64);
+        strcpy(name,"/bin/sh"); 
+        
+         
+        return 0;
+
+}
